fbf53dc3fc26e746865d7edb95d13162ddd6be8c
JaviScriptsWin <javisistemasoperativos@gmail.com>
2024-11-21 23:17:20 +0100

    Update STOPServices25.cmd

--- STOPServices25.cmd
+++ STOPServices25.cmd
@@ -1,7 +1,7 @@
-REM This script specifically stop Windows services and processes that are unnecessary and consume CPU and RAM resources
-REM You can download from CMD command line running, and then execute the batch file STOPServices24.cmd
-
-REM Run:   curl -LJO https://github.com/JaviScriptsWin/Windows-Optimizer/blob/main/STOPServices25.cmd && STOPServices25.cmd
+REM < This script specifically stop Windows 1x services and processes that are unnecessary and consume CPU and RAM resources
+REM < You can download from CMD command line running and then execute the batch file STOPServices24.cmd
+REM < Or  copy and paste the next command line.
+REM Run: ->>  curl -LJO https://raw.githubusercontent.com/JaviScriptsWin/Windows-Optimizer/main/STOPServices25.cmd && stopservices25.cmd     <<-
 
 @echo off
 sc stop PimIndexMaintenanceSvc_8b178
